97dfc8940ab2956a1bebe26e3e7f18112487958f
KJH1212 <90547159+KJH1212@users.noreply.github.com>
2021-11-21 13:19:49 +0900

    Create notiItem.java
    notiItem.java 추가

---
+++ app/src/main/java/com/example/dowith/notiItem.java
@@ -0,0 +1,9 @@
+package com.example.dowith;
+
+public class notiItem {
+    String notiTitle;
+
+    public notiItem(String notiTitle) { this.notiTitle = notiTitle; }
+    public notiItem get() { return this; }
+    public String getItem_title() { return notiTitle; }
+}
